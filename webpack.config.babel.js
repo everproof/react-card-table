a0465128fc8d0f0d561a2c07fdbc31180e7ac2b5
Rhys Gevaux <rhys.gevaux@blueq.com.au>
2017-05-02 15:38:47 +1000

    Installed and configured webpack. Installed sass-lint

---
+++ webpack.config.babel.js
@@ -0,0 +1,91 @@
+const resolve = require('path').resolve
+
+const PATHS = {
+  build: resolve(__dirname, 'dist'),
+  nodeModules: resolve(__dirname, 'node_modules'),
+  src: resolve(__dirname, 'src'),
+}
+
+const reactExternal = {
+  root: 'React',
+  commonjs2: 'react',
+  commonjs: 'react',
+  amd: 'react',
+}
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: PATHS.build,
+    filename: 'index.js',
+    library: 'react-card-table',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        include: [
+          PATHS.src,
+          resolve(PATHS.nodeModules, '@blueq', 'blueq-styles'),
+        ],
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              camelCase: true,
+              importLoaders: 1,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+              modules: true,
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: 'inline',
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        include: [
+          PATHS.src,
+        ],
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      PATHS.nodeModules,
+      PATHS.src,
+    ],
+    extensions: ['.js', '.jsx', '.scss'],
+    enforceExtension: false,
+  },
+  performance: {
+    hints: 'warning',
+    maxAssetSize: 200000,
+    maxEntrypointSize: 400000,
+  },
+  devtool: 'source-map',
+  context: PATHS.src,
+  externals: {
+    react: reactExternal,
+  },
+}
