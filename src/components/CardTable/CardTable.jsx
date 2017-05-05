8c69083387a60291fdeccae64cf36994da0872e9
Rhys Gevaux <rhys.gevaux@blueq.com.au>
2017-05-05 13:38:03 +1000

    Updated webpack. Improved import to reduce bundle size

--- src/components/CardTable/CardTable.jsx
+++ src/components/CardTable/CardTable.jsx
@@ -1,4 +1,4 @@
-import { chunk } from 'lodash'
+import chunk from 'lodash/chunk'
 import { arrayOf, func, node, number, shape, string } from 'prop-types'
 import React, { cloneElement, Component } from 'react'
 
